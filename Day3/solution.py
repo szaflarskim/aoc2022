a51032c144f49bcbd9885debc2910c3b5963b07c
m4csz4f <>
2022-12-03 03:01:48 +0000

    Initial commit

---
+++ Day3/solution.py
@@ -0,0 +1,11 @@
+f=open("Day3/input", "r")
+lines = [line.rstrip() for line in f]
+f.close()
+
+
+def day3():
+  print(lines)
+  
+
+  
+  
---
+++ main.py
@@ -0,0 +1,13 @@
+
+from Day1.solution import day1
+from Day2.solution import day2
+from Day3.solution import day3
+
+if __name__ == "__main__":
+  print("Day 1 =============")
+  day1()
+  print("Day 2 =============")
+  day2()
+  print("Day 3 =============")
+  day3()
+
