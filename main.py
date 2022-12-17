5846b936cb001cb24984d4aaa24a6328aba8be62
szaflarskim <szafmac@chrobinson.com>
2022-12-17 11:46:56 +0000

    shuffle files around

--- main.py
+++ main.py
@@ -1,50 +1,24 @@
-from Day1.solution import day1
-from Day2.solution import day2
-from Day3.solution import day3
-from Day4.solution import day4
-from Day5.solution import day5
-from Day6.solution import day6
-from Day7.solution import day7
-from Day8.solution import day8
-from Day9.solution import day9
-from Day10.solution_v2 import day10
-from Day11.solution import day11
-from Day12.solution import day12
-from Day13.solution import day13
-from Day14.solution import day14
-from Day15.solution import day15
-from Day16.solution import day16
+from src.Day1.solution import day1
+from src.Day2.solution import day2
+from src.Day3.solution import day3
+from src.Day4.solution import day4
+from src.Day5.solution import day5
+from src.Day6.solution import day6
+from src.Day7.solution import day7
+from src.Day8.solution import day8
+from src.Day9.solution import day9
+from src.Day10.solution_v2 import day10
+from src.Day11.solution import day11
+from src.Day12.solution import day12
+from src.Day13.solution import day13
+from src.Day14.solution import day14
+from src.Day15.solution import day15
+from src.Day16.solution import day16
+
+days = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16]
 
 if __name__ == "__main__":
-    print("Day 1 =============")
-    day1()
-    print("Day 2 =============")
-    day2()
-    print("Day 3 =============")
-    day3()
-    print("Day 4 =============")
-    day4()
-    print("Day 5 =============")
-    day5()
-    print("Day 6 =============")
-    day6()
-    print("Day 7 =============")
-    day7()
-    print("Day 8 =============")
-    day8()
-    print("Day 9 =============")
-    day9()
-    print("Day 10 =============")
-    day10()
-    print("Day 11 =============")
-    day11()
-    print("Day 12 =============")
-    day12()
-    print("Day 13 =============")
-    day13()
-    print("Day 14 =============")
-    day14()
-    print("Day 15 =============")
-    day15()
-    print("Day 16 =============")
-    day16()
+    for day in days:
+        print(f"============= Day {days.index(day) + 1} =============")
+        day()
+        print()
---
+++ src/Day16/solution.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+from copy import deepcopy
+import re
+
+"""
+All Valves begin closed;
+
+One minute to open valve
+One minute to walk between valves
+
+"""
+
+
+class Valve:
+    def __init__(self, name, flow_rate, tunnels=[]):
+        self.name = name
+        self.flow_rate = flow_rate
+        self.tunnels = tunnels
+        self.open = False
+
+
+def get_input(input_file):
+    f = open(input_file, "r")
+    lines = [line.rstrip() for line in f]
+    f.close()
+    return lines
+
+def get_valve_map(lines):
+    valve_map = dict()
+    for line in lines:
+        name_str, flow_rate, tunnels_str = [ v for v in re.split('=|;', line)]
+        flow_rate = int(flow_rate)
+        name = name_str.split()[1]
+        tunnels = re.split(" |, ",tunnels_str)[5:]
+        valve_map[name] = Valve(name, flow_rate, tunnels)
+    return valve_map
+
+def day16(input_file="src/Day16/sample"):
+    lines = get_input(input_file)
+    valve_map = get_valve_map(lines)
+
+
+
+
+
+def test():
+    lines = get_input("src/Day16/sample")
+    print(lines)
+
+
+# day16("aoc2022-inputs/d16", p1_row=2000000, limit=4000000)
+
+if __name__ == "__main__":
+    day16()
+# test()
